52136f4f47e9811ef8f39c39c2e369e41b62c18b
Varun Kumar <varu1311@gmail.com>
2023-03-01 18:01:54 +0530

    feat: add theme context

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,20 +1,11 @@
-import React, { useContext, useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { ThemeContext } from '../../contexts/TemeContext';
 import './Navbar.css';
 
 const Navbar = () => {
   const navigate = useNavigate();
-  const { theme, setTheme } = useContext(ThemeContext);
-  const [selectedTheme, setSelectedTheme] = useState({
-    colorHexCode: '#800080',
-  });
-  useEffect(() => {
-    const selected = theme.themes.findIndex((t) => t.id === theme.preferredThemeId);
-    setSelectedTheme(theme.themes[selected]);
-  }, [theme]);
+
   return (
-    <nav style={{ backgroundColor: selectedTheme.colorHexCode }}>
+    <nav>
       <button type="button" onClick={() => navigate('/')} className="logo">
         EVENTIFY
       </button>
